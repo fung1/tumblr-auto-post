40334066cd1700f039f485edf050187cd534cde0
David Godzsak <godzsakdavid@gmail.com>
2015-11-29 20:42:33 +0100

    working but ugly

--- upload.php
+++ upload.php
@@ -1,3 +1,22 @@
 <?php
-  var_dump($_FILES);
+
+   include("./vendor/autoload.php");
+
+   ini_set('display_errors', 'On');
+
+   // Authenticate via OAuth
+   include("./keys.php");
+   $client = new Tumblr\API\Client($a,$b,$c,$d);
+
+
+   var_dump($_FILES);
+   echo json_encode($_FILES["file"]["name"]);
+   // Make the reques
+   $data=array(
+               "data" => array($_FILES["file"]["tmp_name"]),
+               "type"=>"photo",
+               "state"=>'queue',
+               'caption' => 'Photo'
+         );
+   $client->createPost($blogname,$data);
 ?>
